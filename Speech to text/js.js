5f558153e33830bebce88565aeb87e25f9988254
Maldandan <109004035+Maldandan@users.noreply.github.com>
2022-07-10 02:25:58 +0300

    Add files via upload

---
+++ Speech to text/js.js
@@ -0,0 +1,24 @@
+click_to_record.addEventListener('click',function(){
+    var speech = true;
+    window.SpeechRecognition = window.webkitSpeechRecognition;
+
+    const recognition = new SpeechRecognition();
+    recognition.interimResults = true;
+    recognition.continuous = true;
+    recognition.lang = 'ar-AE';
+    recognition.start();
+
+    recognition.addEventListener('result', e => {
+        const transcript = Array.from(e.results)
+            .map(result => result[0])
+            .map(result => result.transcript)
+            .join('')
+
+        document.getElementById("convert_text").innerHTML = transcript;
+        console.log(transcript);
+    });
+    
+    if (speech == true) {
+        recognition.start();
+    }
+})
